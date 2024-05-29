955b7862bc902a47511341b7736b51050863f5f3
plavelo <plastic.velocity@gmail.com>
2024-05-30 00:05:35 +0900

    Add abc150 c

--- abc150/src/bin/c.rs
+++ abc150/src/bin/c.rs
@@ -13,12 +13,26 @@ use std::cmp::{max, min};
 use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
+use num_traits::ToPrimitive;
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        n: usize,
+        ps: [usize; n],
+        qs: [usize; n],
     }
+    let all: Vec<String> = (1..=n)
+        .permutations(n)
+        .map(|x| x
+            .iter()
+            .map(|&y| y.to_string())
+            .join("-")
+        )
+        .collect();
+    let pss = ps.iter().map(|p| p.to_string()).join("-");
+    let qss = qs.iter().map(|q| q.to_string()).join("-");
+    let ppos = all.iter().position(|v| v.to_string() == pss).unwrap().to_i32().unwrap();
+    let qpos = all.iter().position(|v| v.to_string() == qss).unwrap().to_i32().unwrap();
+    println!("{}", (ppos - qpos).abs());
 }
